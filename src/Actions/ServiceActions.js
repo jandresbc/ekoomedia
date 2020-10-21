d42ffb906ee4ed652389d79218e87688ee030e40
Julio Barrera <=>
2020-10-20 19:13:24 -0500

    cambio url

--- src/Actions/ServiceActions.js
+++ src/Actions/ServiceActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const saveData = (data) => async dispatch => {
-    await axios.post("http://127.0.0.1:8000/api/register",data).then((resp) => {
+    await axios.post("https://api-ekoomedia-prueba.herokuapp.com/api/register",data).then((resp) => {
         let data = resp.data;
     
         if(data.code === 200){
